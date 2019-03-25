60dcf1605e20e35120b442a369178a24ff6690d0
Sam Minot <sam@fredhutch.org>
2019-03-25 13:25:09 -0700

    Local execution setup

--- run_nf.sh
+++ run_nf.sh
@@ -3,13 +3,17 @@
 set -e
 
 nextflow \
+    -C "nextflow.local.config" \
     run \
     evaluate_gene_detection.nf \
     --genome_list test_data/test_genomes.csv.gz \
+    --refdb genes.fastp.gz.rep.fasta \
     --outdir nf_outputs \
-    --num_genomes 2 \
+    --num_genomes 1 \
+    --num_simulations 3 \
     --mean_depth 50 \
-    --max_depth 5 \
-    --log_std 1 \
+    --max_depth 10 \
+    --log_std .1 \
     -with-docker "ubuntu:16.04" \
+    -work-dir "work/" \
     -resume
