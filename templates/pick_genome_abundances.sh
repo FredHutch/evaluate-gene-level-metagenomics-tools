0b77b1b57fc3c016ec39dded1f122a68f6c6a34b
Sam Minot <sam@fredhutch.org>
2019-04-25 11:23:39 -0700

    Only take 1 genome per species

--- templates/pick_genome_abundances.sh
+++ templates/pick_genome_abundances.sh
@@ -24,18 +24,39 @@ header = f.readline().rstrip("\\n").split("${sep}")
 assert "RefSeq FTP" in header
 refseq_ftp_ix = header.index("RefSeq FTP")
 
-# Get the complete list of RefSeq FTP paths
-all_genomes = [
-  l.rstrip("\\n").split("${sep}")[refseq_ftp_ix]
-  for l in f
+# Figure out which column has the organism name
+assert "Organism" in header
+organism_ix = header.index("Organism")
+
+# Make a list of the fields in each line
+list_of_lines = [
+    l.rstrip("\\n").split("${sep}")
+    for l in f
 ]
-all_genomes = [g for g in all_genomes if len(g) > 1 and g.startswith('ftp://')]
+
+# Get the organism name and RefSeq FTP path for each genome
+all_genomes = [
+    (l[organism_ix], l[refseq_ftp_ix])
+    for l in list_of_lines
+    if len(l[refseq_ftp_ix]) > 1 and l[refseq_ftp_ix].startswith('ftp://')
+]
 
 print("There are " + str(len(all_genomes)) + " genomes available")
 assert num_genomes <= len(all_genomes)
 
 # Randomly shuffle the genomes
 random.shuffle(all_genomes)
+
+# Only take a single genome per species
+filtered_genomes = []
+species_seen = set([])
+for org_name, ftp_path in all_genomes:
+    species_name = " ".join(org_name.split(" ")[:2]) if len(org_name.split(" ")) > 2 else org_name
+    if species_name not in species_seen:
+        filtered_genomes.append(ftp_path)
+        species_seen.add(species_name)
+
+print("There are " + str(len(filtered_genomes)) + " genomes after removing species duplicates")
 
 # Randomly pick a set of abundances
 log_mean = np.log10(mean_depth)
@@ -54,7 +75,7 @@ abund_list = [
 # Write out to a file
 with open("genome_abund.${ix}.csv", "wt") as fo:
     for k, v in zip(
-      all_genomes[:num_genomes],
+      filtered_genomes[:num_genomes],
       abund_list
     ):
         print(k, v)
